de5deee28fa308cec9d12843025611b21e85d0b0
Anastasia Crespo <anastasiacrespo@Anastasias-MacBook-Pro.local>
2020-04-27 20:56:41 -0400

    create hello world file

---
+++ python-work/learn_lessons.py
@@ -0,0 +1,29 @@
+number1 = 5 # integer
+
+##############################################################################################
+
+# a number with a decimal point is a float
+float2 = 6.3 # floating point number
+float1 = 7. # also a floating point number
+
+##############################################################################################
+
+# anything wrapped in quotes is a string
+string1 = "Hello" # string over here
+string2 = "my name is 29#$%@#$^%" #literally, anything wrapped in quotes is a string
+string3 = "369"
+
+##############################################################################################
+
+# naming variables is important!!! this has a capital "S" in string, so its different
+# than the previous variable of string1
+String1 = "Wonderful!"
+
+##############################################################################################
+
+# MULTIPLE VARIABLES IN ONE VARIABLE?! YEP!
+big_string = string1 + string2
+print(big_string)
+
+cool...now that you read part of a lesson in week one and copy/pasted then ran the code in your own little file, you should
+move on to the todo
